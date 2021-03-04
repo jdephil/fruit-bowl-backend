6211a1c28fa2d2a563b47eec6929a3b35f9e6d19
gunnarsikorski <gunnarsikorski@gmail.com>
2021-03-04 13:39:50 -0600

    Built out index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// CONTROLLERS //
+
+const tipControllers = require('./controllers/tips');
+app.use('/api/tips', tipControllers);
+
+// END CONTROLLERS //
+
+const port = process.env.PORT || 8001;
+
+app.listen(port, () => {
+	console.log(`Fruitbowl-backend is running on port ${port}`);
+});
