868a0087b287b41598db74ab6b90885a3b4afc23
gunnarsikorski <gunnarsikorski@gmail.com>
2021-03-05 13:47:08 -0600

    Adding cors back, troubleshooting heroku

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(cors());
 
 // CONTROLLERS //
 
