8cd8b61e09b657d3c944d22bca70430597b1b77e
gunnarsikorski <gunnarsikorski@gmail.com>
2021-03-05 15:01:07 -0600

    Proper redriect added

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
 
+app.get('/', (req, res) => {
+	res.redirect('/api/tips');
+});
+
 // CONTROLLERS //
 
 const tipControllers = require('./controllers/tips');
