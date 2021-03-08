32a1511b52d0fe42fca5215a6796332082e8d56c
gunnarsikorski <gunnarsikorski@gmail.com>
2021-03-08 10:19:32 -0600

    Attempting solution to 304 error

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.disable('etag');
 
 // app.get('/', (req, res) => {
 // 	res.redirect('/api/tips');
