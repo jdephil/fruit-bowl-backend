24babf2b552a5adc53faa12686a4d5dec8970416
gunnarsikorski <gunnarsikorski@gmail.com>
2021-03-08 10:09:58 -0600

    Now getting empty array on heroku

--- db/tip-seeds.js
+++ db/tip-seeds.js
@@ -3,10 +3,10 @@ const mongoose = require('./connection');
 const Tip = require('../models/tip');
 const seedData = require('./tip-seeds.json');
 
-Tip.deleteMany({})
-	.then(() => {
-		Tip.insertMany(seedData)
-	})
+// Tip.deleteMany({})
+// .then(() => {
+
+Tip.insertMany(seedData)
 	.then(console.log())
 	.catch(console.error)
 	.finally(() => {
