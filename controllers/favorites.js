5c77d1772607c9ae68fa87f01de380c5a7381e68
jdephil <jennifer.dephillips@gmail.com>
2021-03-17 23:02:01 -0400

    rendering faves in progress

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -1,13 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('../db/connection')
 
 const Tip = require('../models/tip');
 
-router.post('/', (req, res, next) => {
-	const tipData = req.body;
-	Tip.findById(tipData._id)
-		.then((tip) => res.status(201).json(tip))
-		.catch(next);
+router.post('/', async (req, res, next) => {
+	console.log("tip",Tip)
+	let faveQuery = []
+	for (let i = 0; i < req.body.length; i++) {
+		console.log(req.body[i])
+		console.log("to string", req.body[i].toString())
+		// let mongoId = mongoose.Types.ObjectId(req.body[i])
+		// faveQuery.push(mongoId)
+		// let faveTip = await Tip.find({
+		//  '_id': { $in: [
+		// 		// mongoose.Types.ObjectId(req.body[i].toString())
+		// 		mongoose.Types.ObjectId('60464ee6b1ee8600040797e9')
+		//  ]} 
+		// }		
+		// 		// '60464ee6b1ee8600040797e9'
+		// )
+
+		let faveTip = await Tip.find({
+			'_id': { $in: [
+					mongoose.Types.ObjectId('60464ee6b1ee8600040797df')
+					
+			]}
+	});
+		console.log("faveTip", faveTip)
+		faveQuery.push(faveTip)
+	}
+	console.log("faveQuery", faveQuery)
+	// console.log("favequery", faveQuery)
+	//console.log(req.body)
+	// for (var fave in req.body) {
+		// 	console.log(fave)
+		// 	// console.log(mongoose.Types.ObjectId(`${fave}`))
+		// 	// let mongoId = mongoose.Types.ObjectId(`${fave}`)
+		// 	// faveQuery.push(mongoId)
+		// }
+		// console.log(faveQuery)
+		// .then((tip) => 
+
+
+	res.status(201).json({tip: faveQuery})
+	
+	//)
+	// .catch(next);
 });
 
 router.delete('/:id', (req, res, next) => {
